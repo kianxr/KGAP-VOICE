33b2d3ee5c5115a32f3c45e24064e26a8f97e229
kianxr <kianghasemi803@gmail.com>
2026-07-23 01:29:03 +0330

    Create Roles.js

---
+++ Roles.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const {
+getRole,
+setRole
+}=require("./roles");
+
+let users =
+require("./users.json");
+
+
+function getRole(username){
+
+    if(users[username]){
+        return users[username].role;
+    }
+
+    return "member";
+
+}
+
+
+
+function setRole(username,role){
+
+    users[username]={
+        role:role
+    };
+
+
+    fs.writeFileSync(
+        "./users.json",
+        JSON.stringify(users,null,4)
+    );
+
+}
+
+
+module.exports={
+    getRole,
+    setRole
+};
