1454538d640e4bd3256d7f12efaff5cf60c933a2
kianxr <kianghasemi803@gmail.com>
2026-07-23 00:51:56 +0330

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(cors());
+
+
+const server = http.createServer(app);
+
+
+const io = new Server(server,{
+    cors:{
+        origin:"*"
+    }
+});
+
+
+let rooms = {};
+
+
+io.on("connection",(socket)=>{
+
+
+console.log("User connected");
+
+
+socket.on("join-room",(room,username)=>{
+
+
+socket.join(room);
+
+
+if(!rooms[room])
+rooms[room]=[];
+
+
+rooms[room].push({
+id:socket.id,
+name:username
+});
+
+
+io.to(room).emit(
+"users",
+rooms[room]
+);
+
+
+});
+
+
+
+socket.on("signal",(data)=>{
+
+io.to(data.target)
+.emit(
+"signal",
+{
+sender:socket.id,
+signal:data.signal
+}
+);
+
+});
+
+
+
+socket.on("disconnect",()=>{
+
+for(let room in rooms){
+
+rooms[room]=rooms[room]
+.filter(
+u=>u.id!==socket.id
+);
+
+io.to(room)
+.emit(
+"users",
+rooms[room]
+);
+
+}
+
+});
+
+
+});
+
+
+server.listen(3000,()=>{
+
+console.log(
+"KCRAFT Voice Server Running"
+);
+
+});
